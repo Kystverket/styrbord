1805a3801d990eea1b20cbeeadd4da02a9d600f1
eksepsjon <380919+eksepsjon@users.noreply.github.com>
2023-09-13 13:18:04 +0200

    More Kystverket in the storybook theme

--- .storybook/kyv-theme.ts
+++ .storybook/kyv-theme.ts
@@ -6,4 +6,29 @@ export default create({
   brandUrl: 'https://kystverket.no',
   brandImage: 'https://kystverket.no/UI/Icons/logo.svg',
   brandTarget: '_self',
+
+  //
+  colorPrimary: '#000667',
+  colorSecondary: '#ff451f',
+
+  // UI
+  appBg: '#ffffff',
+  appContentBg: '#ffffff',
+  appBorderColor: '#000667',
+  appBorderRadius: 0,
+
+  // Text colors
+  textColor: '#000667',
+  textInverseColor: '#ffffff',
+
+  // Toolbar default and active colors
+  barTextColor: '#9E9E9E',
+  barSelectedColor: '#585C6D',
+  barBg: '#ffffff',
+
+  // Form colors
+  inputBg: '#ffffff',
+  inputBorder: '#000667',
+  inputTextColor: '#000667',
+  inputBorderRadius: 0,
 });
