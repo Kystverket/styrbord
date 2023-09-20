eee66365c6fe4e4e0f5709b0e8daa3b9d66291b5
Marius Kalvø <marius.kalvo@kystverket.no>
2023-09-20 08:22:36 +0200

    Added checkbox component

--- src/components/StyrbordProvider/StyrbordProvider.tsx
+++ src/components/StyrbordProvider/StyrbordProvider.tsx
@@ -1,13 +1,39 @@
 import '../../styles/variables.css';
 
 import React from 'react';
-import { ChakraProvider, ChakraProviderProps } from "@chakra-ui/react";
+import { ChakraProvider, ChakraProviderProps, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+    colors: {
+        navy: "#000667",
+        rust: "#ff451f",
+        sky: "#a9e5fb",
+        sand: "#f9f6f3",
+        sea: "#0596cb",
+        grey: "#5d5d5d",
+        forest: "#3ba460",
+        colorSchemes: {
+            navy: {
+                100: "#aeb2ff",
+                200: "#858cff",
+                300: "#5c66ff",
+                400: "#333fff",
+                500: "#0b19ff",
+                600: "#000de1",
+                700: "#000bb8",
+                800: "#000890",
+                900: "#000667",
+            }
+        }
+    },
+});
+
 /**
  * StyrbordProvider er komponenten som gir de andre komponentene riktig tema og stil.
  * Se https://chakra-ui.com/getting-started/cra-guide#2-provider-setup.
  */
 export const StyrbordProvider = ({ children, ...props }: ChakraProviderProps) => (
-    <ChakraProvider {...props}>
+    <ChakraProvider {...props} theme={theme}>
         {children}
     </ChakraProvider>
 );
