b5b1364b0eb0fb4500b46b58b820cb5e5bedd80e
Magnus Ekse <380919+eksepsjon@users.noreply.github.com>
2024-09-25 15:00:51 +0200

    Test av workflow

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
 import './css/index.scss';
 
-export * from '@digdir/designsystemet-react';
+export { Button, Link } from '@digdir/designsystemet-react';
+
+export { default as Alert } from './components/Alert/alert';
+export { default as Box } from './components/Box/box';
+export { default as Icon } from './components/Icon/icon';
+export * from './components/Typography/typography';
