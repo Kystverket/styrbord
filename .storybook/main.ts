ff8c1f42f1ab1b304e423b239d5a9ea79d88a7b5
Magnus Ekse <380919+eksepsjon@users.noreply.github.com>
2024-09-25 15:20:02 +0200

    Storybook upgrade

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../storybook/**/*.mdx', '../storybook/**/*.stories.@(ts|tsx)', '../src/components/**/*.stories.@(ts|tsx)'],
+
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
@@ -9,10 +10,18 @@ const config: StorybookConfig = {
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
   ],
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
+
   staticDirs: ['../assets'],
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+  },
 };
 export default config;
