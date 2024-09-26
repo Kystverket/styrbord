83c47a1fc0e0651ac493a3dbd10dd82ee960c204
Magnus Ekse <380919+eksepsjon@users.noreply.github.com>
2024-09-26 09:56:37 +0200

    Expose alert level

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import './css/index.scss';
 
 export { default as Alert } from './components/Alert/alert';
+export type { AlertLevel } from './components/Alert/alert.types';
 export type { AlertProps } from './components/Alert/alert';
+
 export { default as Box } from './components/Box/box';
 export type { BoxProps } from './components/Box/box';
+
 export { default as Icon } from './components/Icon/icon';
 
 export { Display, Headline, Title, Body, Label } from './components/Typography/typography';
