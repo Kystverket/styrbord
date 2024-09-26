5b37af62a40e085702a4e6e2c4af30b1e249e72b
Magnus Ekse <380919+eksepsjon@users.noreply.github.com>
2024-09-26 08:52:16 +0200

    Bytte rekkefølge for å kunne eksporte hele dsno

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import './css/index.scss';
 
-export { Button, Link } from '@digdir/designsystemet-react';
-
 export { default as Alert } from './components/Alert/alert';
 export { default as Box } from './components/Box/box';
 export { default as Icon } from './components/Icon/icon';
-export * from './components/Typography/typography';
+
+export { Label, Headline, Title, Body } from './components/Typography/typography';
+
+export * from '@digdir/designsystemet-react';
