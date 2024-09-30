11b57707a0302d782016f3177116655cb7c32ee0
Magnus Ekse <380919+eksepsjon@users.noreply.github.com>
2024-09-30 12:28:02 +0200

    ErrorLabelWrapper story plassering

--- src/components/ErrorLabelWrapper/errorLabelWrapper.stories.tsx
+++ src/components/ErrorLabelWrapper/errorLabelWrapper.stories.tsx
@@ -1,13 +1,13 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import ErrorLabelWrapper, { ErrorLabelWrapperProps } from "./errorLabelWrapper";
-import { Textfield } from "@digdir/designsystemet-react";
-import StyrbordDecorator from "../../../storybook/styrbordDecorator";
+import type { Meta, StoryObj } from '@storybook/react';
+import ErrorLabelWrapper, { ErrorLabelWrapperProps } from './errorLabelWrapper';
+import { Textfield } from '@digdir/designsystemet-react';
+import StyrbordDecorator from '../../../storybook/styrbordDecorator';
 
 const meta = {
-  title: "Form/ErrorWrapper",
+  title: 'Components/ErrorLabelWrapper',
   component: ErrorLabelWrapper,
   decorators: [StyrbordDecorator],
-  tags: ["autodocs"],
+  tags: ['autodocs'],
   argTypes: {},
 } satisfies Meta<typeof ErrorLabelWrapper>;
 
@@ -16,7 +16,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 const defaultProps: ErrorLabelWrapperProps = {
-  error: "I am an error",
+  error: 'I am an error',
   children: <Textfield />,
 };
 
