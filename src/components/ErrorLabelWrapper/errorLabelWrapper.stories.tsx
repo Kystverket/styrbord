2506bc1ed4d34512a520343de8070e95f20a277f
Magnus Ekse <380919+eksepsjon@users.noreply.github.com>
2024-09-30 10:50:01 +0200

    Refactor error label

---
+++ src/components/ErrorLabelWrapper/errorLabelWrapper.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ErrorLabelWrapper, { ErrorLabelWrapperProps } from './errorLabelWrapper';
+import { Textfield } from '@digdir/designsystemet-react';
+import StyrbordDecorator from '../../../storybook/styrbordDecorator';
+
+const meta = {
+  title: 'Form/ErrorWrapper',
+  component: ErrorLabelWrapper,
+  decorators: [StyrbordDecorator],
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof ErrorLabelWrapper>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const defaultProps: ErrorLabelWrapperProps = {
+  error: 'I am an error',
+  children: <Textfield />,
+};
+
+export const Default: Story = {
+  args: defaultProps,
+};
+
+export const NoError: Story = {
+  args: { ...defaultProps, error: undefined },
+};
---
+++ src/components/ErrorLabelWrapper/errorLabelWrapper.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react';
+import style from './errorLabelWrapper.module.css';
+import { Label } from '../Typography/typography';
+import Icon from '../Icon/icon';
+import Box from '../Box/box';
+
+export interface ErrorLabelWrapperProps {
+  error?: string | null;
+  children?: ReactNode;
+}
+
+const ErrorLabelWrapper = (props: ErrorLabelWrapperProps) => {
+  const errorHasText = typeof props.error === 'string' && props.error.length > 0;
+  return (
+    <Box gap={1} className={errorHasText ? style.withErrors : style.noErrors}>
+      <Box>{props.children}</Box>
+      <Box horizontal align="center" gap={1} className={style.error}>
+        <Icon material="error" filled className={style.icon} />
+        <Label.Strong.Small className={`${style.text}`}>{props.error}</Label.Strong.Small>
+      </Box>
+    </Box>
+  );
+};
+
+export default ErrorLabelWrapper;
