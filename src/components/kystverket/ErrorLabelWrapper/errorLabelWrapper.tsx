def625638ce0eda4649116d2d241246f9ba5e1e7
Magnus Ekse <380919+eksepsjon@users.noreply.github.com>
2025-01-22 14:00:14 +0100

    Tillate align i label wrapper

--- src/components/kystverket/ErrorLabelWrapper/errorLabelWrapper.tsx
+++ src/components/kystverket/ErrorLabelWrapper/errorLabelWrapper.tsx
@@ -7,15 +7,16 @@ import Box from '../Box/box';
 export interface ErrorLabelWrapperProps {
   error?: string | null;
   children?: ReactNode;
+  align?: 'start' | 'center';
 }
 
-const ErrorLabelWrapper = (props: ErrorLabelWrapperProps) => {
+const ErrorLabelWrapper = ({ align = 'center', ...props }: ErrorLabelWrapperProps) => {
   const errorHasText = typeof props.error === 'string' && props.error.length > 0;
   return (
     <Box gap={4}>
       <div>{props.children}</div>
       {errorHasText && (
-        <Box horizontal align="center" gap={4} className={style.error}>
+        <Box horizontal align={align} gap={4} className={style.error}>
           <Icon material="error" filled className={style.icon} />
           <Label size="sm" strong className={style.text}>
             {props.error}
