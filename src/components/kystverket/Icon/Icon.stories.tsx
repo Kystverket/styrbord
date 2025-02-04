9bcd395a25fe0fcf9971b599999c88036997ae7b
Mathias Mølster Lidal <mathias.lidal@bouvet.no>
2025-02-04 13:37:03 +0100

    Legg til storybook

---
+++ src/components/kystverket/Icon/Icon.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import StyrbordDecorator from '../../../../storybook/styrbordDecorator';
+import Icon, { MaterialIconProps } from '~/components/kystverket/Icon/icon';
+
+const meta = {
+  title: 'Components/Icon',
+  component: Icon,
+  decorators: [StyrbordDecorator],
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof Icon>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const defaultProps: MaterialIconProps = {
+  material: 'chevron_left'
+};
+
+export const Default: Story = {
+  args: defaultProps,
+};
+
