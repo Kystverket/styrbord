f74a6a098987d53a724baaef6fe838255298a720
sirisandnes <siri.borgen.sandnes@kystverket.no>
2025-03-25 12:11:21 +0100

    prettier

--- src/components/kystverket/Footer/Footer.tsx
+++ src/components/kystverket/Footer/Footer.tsx
@@ -62,14 +62,14 @@ export function Footer({ links = [], langLinks = [], language, contactLinks }: F
               </Select.Option>
               {contactLinks
                 ? contactLinks.map((link: LinkToSite, index: number) => (
-                    <Select.Option key={index} value={link.url} className={classes.selectOption}>
-                      {link.text}
-                    </Select.Option>
+                  <Select.Option key={index} value={link.url} className={classes.selectOption}>
+                    {link.text}
+                  </Select.Option>
                   ))
                 : defaultContactLinks.map((link: LinkToSite, index: number) => (
-                    <Select.Option key={index} value={link.url} className={classes.selectOption}>
-                      {link.text}
-                    </Select.Option>
+                  <Select.Option key={index} value={link.url} className={classes.selectOption}>
+                    {link.text}
+                  </Select.Option>
                   ))}
             </Select>
             © Kystverket
