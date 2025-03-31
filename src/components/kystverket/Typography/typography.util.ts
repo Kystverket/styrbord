77b10963f68067aa7f25e993d4747dd2a4c56c03
Mathias Mølster Lidal <mathias.lidal@bouvet.no>
2025-03-31 12:27:03 +0200

    MS-339 Oppdater body-stiler for maritim sikring
    Legg til ekstra valg for body for å passe med endra tekststiler for maritim sikring.
    - Lagt til størrelse xl og xxl
    - Lag til valg for margin etter body
    - Lagt til valg for kystverket blå farge på tekst

--- src/components/kystverket/Typography/typography.util.ts
+++ src/components/kystverket/Typography/typography.util.ts
@@ -1,14 +1,18 @@
 import typography from './typography.module.css';
 
-export type TypographyPrefix = 'display' | 'headline' | 'title' | 'body' | 'label' | 'accent';
+export type TypographyPrefix = 'display' | 'headline' | 'title' | 'body' | 'label' | 'accent' | 'paragraph';
 
-export type AllSizes = 'sm' | 'md' | 'lg' | 'xl';
+export type AllSizes = 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
+
+export type TypographyColor = 'accent' | 'neutral';
 
 type BuildTypographyProps = {
   type: TypographyPrefix;
   size?: AllSizes;
   strong?: boolean;
   inline?: boolean;
+  color?: TypographyColor;
+  margin?: boolean;
   className?: string;
 };
 
@@ -17,12 +21,16 @@ export const buildTypographyClasses = ({
   size = 'md',
   strong = false,
   inline = false,
+  color = 'neutral',
+  margin = false,
   className,
 }: BuildTypographyProps): string => {
   return [
     typography[type],
     typography[`${type}-${size}${strong ? '-strong' : ''}`],
     inline ? typography.inline : '',
+    color === 'accent' ? typography.accentColor : '',
+    margin ? typography.margin : '',
     className ?? '',
   ].join(' ');
 };
