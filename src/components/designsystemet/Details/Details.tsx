9d57d160281ad9b328fb051b835b4c5b29e19673
Magnus Ekse <380919+eksepsjon@users.noreply.github.com>
2025-05-07 12:35:35 +0200

    Forenkle details

--- src/components/designsystemet/Details/Details.tsx
+++ src/components/designsystemet/Details/Details.tsx
@@ -1,34 +1,20 @@
-import { Card, Details as DsDetails } from '@digdir/designsystemet-react';
+import {
+  Details as DsDetails,
+  DetailsProps as DsDetailsProps,
+  DetailsSummary as DsDetailsSummary,
+  DetailsContent as DsDetailsContent,
+} from '@digdir/designsystemet-react';
 
-export interface DetailsProps {
-  wrapped?: boolean;
-  classname?: string;
+export type DetailsProps = DsDetailsProps & {
   summary: React.ReactNode;
   children: React.ReactNode;
-  variant?: 'default' | 'tinted';
-  defaultOpen?: boolean;
-  onToggle?: (event: Event) => void;
-}
+};
 
-export const Details = ({
-  wrapped = false,
-  summary,
-  children,
-  variant = 'default',
-
-  defaultOpen = false,
-  ...props
-}: DetailsProps) => {
-  const details = (
-    <DsDetails defaultOpen={defaultOpen} onToggle={props.onToggle} variant={variant}>
-      <DsDetails.Summary>{summary}</DsDetails.Summary>
-      <DsDetails.Content>{children}</DsDetails.Content>
+export const Details = ({ summary, children, ...props }: DetailsProps) => {
+  return (
+    <DsDetails {...props}>
+      <DsDetailsSummary>{summary}</DsDetailsSummary>
+      <DsDetailsContent>{children}</DsDetailsContent>
     </DsDetails>
   );
-
-  if (wrapped) {
-    return <Card>{details}</Card>;
-  } else {
-    return details;
-  }
 };
