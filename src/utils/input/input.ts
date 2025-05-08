5d6bfcca7e9dec959b0c72525ff3c7008749063c
sirisandnes <siri.borgen.sandnes@kystverket.no>
2025-05-08 13:15:10 +0200

    versioning +

--- src/utils/input/input.ts
+++ src/utils/input/input.ts
@@ -2,7 +2,7 @@ import styles from './input.module.css';
 
 export type InputSize = '2xs' | 'xs' | 'sm' | 'md' | 'lg' | 'fit' | 'full';
 
-export const inputSizeClass = (size: string) => {
+export const inputSizeClass = (size: InputSize) => {
   switch (size) {
     case '2xs':
       return styles.sizeXxs;
