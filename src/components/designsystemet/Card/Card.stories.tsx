241f88e2522c5998eb2d5cb4eaed8e83475d31e7
Magnus Ekse <380919+eksepsjon@users.noreply.github.com>
2025-10-23 12:38:05 +0200

    Flere farger i story, bedre override, grå default

--- src/components/designsystemet/Card/Card.stories.tsx
+++ src/components/designsystemet/Card/Card.stories.tsx
@@ -1,8 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import StyrbordDecorator from '../../../../storybook/styrbordDecorator';
-import { Card } from '@digdir/designsystemet-react';
+import { Card, CardBlock, CardProps } from '@digdir/designsystemet-react';
 import CardTitle from '../CardTitle/CardTitle';
+import { Box } from '~/main';
 
 const meta = {
   title: 'Designsystemet/Card',
@@ -28,7 +29,44 @@ export const Default: Story = {
   },
 };
 
-export const Tinted: Story = {
+const colors: CardProps['data-color'][] = [
+  undefined,
+  'primary',
+  'neutral',
+  'success',
+  'danger',
+  'warning',
+  'accent',
+  'extra1',
+  'extra2',
+];
+
+const colorVariants: CardProps['variant'][] = ['default', 'tinted'];
+
+export const ColorVariants: Story['render'] = () => (
+  <Box gap={16}>
+    {colors.map((color) =>
+      colorVariants.map((colorVariant) => (
+        <Box key={color + colorVariant} horizontal align="center" gap={8}>
+          <Card data-color={color} variant={colorVariant}>
+            <CardBlock>
+              <CardTitle>
+                {color ? color.toString().substring(0, 1).toUpperCase() + color.toString().substring(1) : 'Default'}{' '}
+                {colorVariant}
+              </CardTitle>
+              <p>
+                Most provide as with carried business are much better more the perfected designer. Writing slightly
+                explain desk unable at supposedly about this
+              </p>
+            </CardBlock>
+          </Card>
+        </Box>
+      )),
+    )}
+  </Box>
+);
+
+export const Colors: Story = {
   args: {
     children: <p>Dette er et tinted kort med standardinnhold.</p>,
     variant: 'tinted',
