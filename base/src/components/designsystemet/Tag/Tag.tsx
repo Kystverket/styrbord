b929d154c102b0f194e18171dcc79b564a08e39f
Siri Sandnes <98953954+siribsandnes@users.noreply.github.com>
2026-03-04 09:14:30 +0100

    rounded borders (#158)
    * rounded borders
    
    * fjern dist
    
    * dist i gitignore
    
    * prettier
    
    * La til story for rounded tags med alle fargevarianter
    
    ---------
    
    Co-authored-by: Thomas Lamborg <thomas.lamborg@twoday.com>

--- base/src/components/designsystemet/Tag/Tag.tsx
+++ base/src/components/designsystemet/Tag/Tag.tsx
@@ -3,12 +3,16 @@ import classes from './Tag.module.css';
 
 export type TagProps = DsTagProps & {
   bordered?: boolean;
+  rounded?: boolean;
 };
 
-const Tag = ({ bordered = false, className = '', ...props }: TagProps) => {
+const Tag = ({ bordered = false, rounded = false, className = '', ...props }: TagProps) => {
   const classNames = [className];
   if (bordered) {
     classNames.push(classes.bordered);
+  }
+  if (rounded) {
+    classNames.push(classes.rounded);
   }
   return <DsTag className={classNames.join(' ')} {...props} />;
 };
