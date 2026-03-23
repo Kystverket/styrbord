1ca8119a266a6087beeaf6f2f5568018ddc43ebc
jnlstad <jan.elling.h@gmail.com>
2026-03-23 13:17:31 +0000

    Deploying to gh-pages from @ Kystverket/styrbord@ca099a876574ec13c57100bfac16ee15c5005fc2 🚀

---
+++ base/assets/errorLabel.stories-CXRiPpdi.js
@@ -0,0 +1,8 @@
+import{j as c}from"./jsx-runtime-D_zvdyIk.js";import{aD as n}from"./SlotDialog-BjxFYPT4.js";import{S as d}from"./styrbordDecorator-a-WuWX2r.js";import"./iframe-oAFnWxx_.js";import"./preload-helper-Dp1pzeXC.js";import"./Details-CkUgoja7.js";import"./index-DGOaacX7.js";import"./lite-DaUVFjkg.js";import"./translations-CeY4WNvS.js";import"./kyvDivider-GkhM6YIE.js";import"./typography-CdiQYpGP.js";import"./skillingsbuoye-BG85ofhS.js";import"./Logo-DajRw6N7.js";import"./index-DErrtZSD.js";import"./index-CmLx86Cz.js";import"./index-MMUsqpFj.js";import"./heading-T7k0LBQ0.js";import"./label-Dj2GAQxs.js";import"./index-dduiamfj.js";const R={title:"Deprecated/ErrorLabel",component:n,decorators:[d],tags:["autodocs","deprecated"],argTypes:{}},i={error:"I am an error",children:c.jsx("input",{type:"text"})},r={args:i},o={args:{...i,error:void 0}};var t,e,a;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: defaultProps
+}`,...(a=(e=r.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};var s,p,m;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    ...defaultProps,
+    error: undefined
+  }
+}`,...(m=(p=o.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const T=["Default","NoError"];export{r as Default,o as NoError,T as __namedExportsOrder,R as default};
--- base/assets/index-D2_lJmrK.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-BpTrYzJe.js";import{r as o}from"./index-lru3SYWb.js";var t=o();const m=r(t);export{m as R,t as r};
