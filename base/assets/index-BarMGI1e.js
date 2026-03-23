3ff3f7939520325555b43b4573bfd4014a91e3a7
eksepsjon <380919+eksepsjon@users.noreply.github.com>
2026-03-23 09:31:27 +0000

    Deploying to gh-pages from @ Kystverket/styrbord@e9f0b756125c2a49b552bbcdb14248cc622172ca 🚀

---
+++ base/assets/index-BarMGI1e.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-DyCle3v5.js";import{r as o}from"./index-D28tee1G.js";var t=o();const m=r(t);export{m as R,t as r};
---
+++ base/assets/richTextArea.stories-Bt0-20zn.js
@@ -0,0 +1,17 @@
+import{j as k}from"./jsx-runtime-D_zvdyIk.js";import{r as x}from"./iframe-DyCle3v5.js";import{S as D}from"./styrbordDecorator-BoxBvY-z.js";import{al as v}from"./SlotDialog-fU0bMQ7e.js";import"./preload-helper-Dp1pzeXC.js";import"./translations-TmhdjV-B.js";import"./Details-BK17jDBD.js";import"./index-DaXf--BG.js";import"./lite-DaUVFjkg.js";import"./kyvDivider-GkhM6YIE.js";import"./typography-CdiQYpGP.js";import"./skillingsbuoye-BG85ofhS.js";import"./Logo-DajRw6N7.js";import"./index-BarMGI1e.js";import"./index-D28tee1G.js";import"./index-D25NBWRS.js";import"./heading-BGqmBN5s.js";import"./label-DZDPmgvA.js";import"./index-DDI5JjEr.js";const J={title:"Form/RichTextArea/RichTextArea",component:v,decorators:[D],tags:["autodocs","kyv","beta"]},a={value:"",onChange:()=>{},label:"Rikt tekstfelt",description:"Dette er et tekstfelt som støtter rik tekstformatering.",optional:"Valgfritt"},s=o=>{const[h,b]=x.useState(o.value??"");return k.jsx(v,{...o,value:h,onChange:n=>{b(n),o.onChange(n)}})},e={args:a,render:s},r={args:{...a,disabled:!0,value:"Skrivebeskyttet innhold"},render:s},t={args:{...a,error:"Du må fylle ut dette feltet."},render:s};var i,m,c;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: defaultArgs,
+  render: renderInteractive
+}`,...(c=(m=e.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};var d,p,l;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    ...defaultArgs,
+    disabled: true,
+    value: 'Skrivebeskyttet innhold'
+  },
+  render: renderInteractive
+}`,...(l=(p=r.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var u,f,g;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    ...defaultArgs,
+    error: 'Du må fylle ut dette feltet.'
+  },
+  render: renderInteractive
+}`,...(g=(f=t.parameters)==null?void 0:f.docs)==null?void 0:g.source}}};const K=["Default","Disabled","WithError"];export{e as Default,r as Disabled,t as WithError,K as __namedExportsOrder,J as default};
