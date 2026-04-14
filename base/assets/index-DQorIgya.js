9f1685f2b976aa29c1ac4533b5699ee76b8c7309
eksepsjon <380919+eksepsjon@users.noreply.github.com>
2026-04-14 13:01:43 +0000

    Deploying to gh-pages from @ Kystverket/styrbord@bc05ddd7f1bc4f5db65a8a01a4a7046fb2bc452d 🚀

---
+++ base/assets/index-DQorIgya.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-CCmo06Ot.js";import{r as o}from"./index-BNTvVDpB.js";var t=o();const m=r(t);export{m as R,t as r};
---
+++ base/assets/svgImage.stories-Cjak7ph_.js
@@ -0,0 +1,26 @@
+import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{S as f}from"./styrbordDecorator-Bqj8vWOR.js";import{b as c,c as F,s as h,d as y,e as u,f as j,g as b,h as B,i as N,j as x,k as O,l as K,m as w,n as U,o as D,p as R,q as A,r as H,t as T,u as _,v as E,w as L,x as M,y as z,z as P,C,D as q,E as G,F as J,G as Q,J as V,K as W,L as X,M as Y,N as Z,O as $,Q as aa,R as ea,S as sa,U as ra,V as ta,W as ga,X as na,Y as ma,Z as ia,_ as oa,$ as Ia,a0 as la,a1 as da,a2 as va,a3 as pa,a4 as ca,a5 as Fa}from"./SlotDialog-DC_ZdIh1.js";import"./Details-D-kSN_Sd.js";import"./translations-C4IlanDA.js";import"./kyvDivider-GkhM6YIE.js";import{a as ua}from"./typography-CdiQYpGP.js";import"./skillingsbuoye-BG85ofhS.js";import"./Logo-DajRw6N7.js";import"./iframe-CCmo06Ot.js";import"./preload-helper-Dp1pzeXC.js";import"./index-DHA6JEJB.js";import"./lite-DaUVFjkg.js";import"./index-DQorIgya.js";import"./index-BNTvVDpB.js";import"./index-D0LssxAE.js";import"./heading-CwIomwvw.js";import"./label-C3szw36r.js";import"./index-DhhjbttY.js";const Ea={title:"Helpers/Images and Illustrations",component:c,decorators:[f],tags:["autodocs","kyv"],argTypes:{}},ka=[["ImageAisSat",y],["ImageArcticInfo",u],["ImageAvgiftskalkulator",j],["ImageBaatfartMarine",b],["ImageBestilleLos",B],["ImageBoelgevarsel",N],["ImageBwArcticinfo",x],["ImageBwBoelgevarsel",O],["ImageBwNais",K],["ImageBwOhoi",w],["ImageDatakatalog",U],["ImageDigitaleFyrbesoek",D],["ImageDigitaleReferanseruter",R],["ImageEDialog",A],["ImageForespoerselSendt",H],["ImageHais",T],["ImageHavbase",_],["ImageHavnOgFarvannsloven",E],["ImageHavnOgFarvannslovenFarget",L],["ImageIsmelding",M],["ImageIsmeldingFarget",z],["ImageKikkert",P],["ImageKystdatahuset",C],["ImageKystinfoKart",q],["ImageKystreise",G],["ImageKystvaer",J],["ImageLavutslipp",Q],["ImageMarU",V],["ImageMegafon",W],["ImageNais",X],["ImageNavarea",Y],["ImageNavigasjonsvarsler",Z],["ImageNavigasjonsvarslerFarget",$],["ImageOhoi",aa],["ImageRegistrerteSeilaser",ea],["ImageSafeSeaNet",sa],["ImageSeLosbestilling",ra],["ImageSelvbetjening",ta],["ImageSelvbetjeningRso",ga],["ImageSelvbetjeningRsoFarget",na],["ImageSlukkedeFyrlys",F],["ImageSoeknadOmFarledsbevis",c],["ImageFarvannsskiltOgNavinnretninger",ma],["ImageFarvannsskiltOgNavinnretningerFarget",ia],["ImageSkadePaaInfrastruktur",oa],["ImageSkadePaaInfrastrukturFarget",Ia],["ImageTilskuddTilFiskerihavn",la],["ImageTilskuddTilFiskerihavnFarget",da],["ImageUtfordringIFarvannet",va],["ImageUtfordringIFarvannetFarget",pa],["ImageUtbedringIFarvannet",ca],["ImageUtbedringIFarvannetFarget",Fa]],g=[...h],e=()=>a.jsxs("table",{children:[a.jsx("thead",{children:a.jsxs("tr",{children:[a.jsx("th",{}),g.map(t=>a.jsx("th",{style:{padding:"0.5rem"},children:t}))]})}),ka.map(([t,k])=>a.jsxs("tr",{children:[a.jsx("td",{style:{padding:"0.5rem"},children:a.jsxs(ua,{strong:!0,children:["<",t," />"]})}),g.map(S=>a.jsx("td",{style:{padding:"0.5rem"},children:a.jsx(k,{"data-color":S})}))]},t))]}),s=()=>a.jsx(F,{size:60}),r=()=>a.jsx(u,{size:8});e.__docgenInfo={description:"",methods:[],displayName:"Showcase"};s.__docgenInfo={description:"",methods:[],displayName:"Big"};r.__docgenInfo={description:"",methods:[],displayName:"Small"};var n,m,i;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`() => {
+  return <table>
+      <thead>
+        <tr>
+          <th></th>
+          {variants.map(variant => <th style={{
+          padding: '0.5rem'
+        }}>{variant}</th>)}
+        </tr>
+      </thead>
+      {images.map(([elementName, Element]) => <tr key={elementName}>
+          <td style={{
+        padding: '0.5rem'
+      }}>
+            <Body strong>&lt;{elementName} /&gt;</Body>
+          </td>
+          {variants.map(variant => <td style={{
+        padding: '0.5rem'
+      }}>{<Element data-color={variant} />}</td>)}
+        </tr>)}
+    </table>;
+}`,...(i=(m=e.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};var o,I,l;s.parameters={...s.parameters,docs:{...(o=s.parameters)==null?void 0:o.docs,source:{originalSource:`() => {
+  return <ImageSlukkedeFyrlys size={60} />;
+}`,...(l=(I=s.parameters)==null?void 0:I.docs)==null?void 0:l.source}}};var d,v,p;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`() => {
+  return <ImageArcticInfo size={8} />;
+}`,...(p=(v=r.parameters)==null?void 0:v.docs)==null?void 0:p.source}}};const La=["Showcase","Big","Small"];export{s as Big,e as Showcase,r as Small,La as __namedExportsOrder,Ea as default};
