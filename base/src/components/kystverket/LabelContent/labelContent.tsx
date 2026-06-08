52f8ca3bfa3682a122d2db5fa157afeac382c282
Thomas Lamborg <thomas.lamborg@twoday.com>
2026-06-08 09:45:25 +0200

    Added missing translation to optional tag

--- base/src/components/kystverket/LabelContent/labelContent.tsx
+++ base/src/components/kystverket/LabelContent/labelContent.tsx
@@ -1,5 +1,6 @@
 import style from './labelContent.module.css';
 import { Spinner, Tag } from '~/main';
+import { useTranslation } from '~/translations';
 
 export interface LabelContentProps {
   optional?: boolean | string | undefined;
@@ -9,6 +10,8 @@ export interface LabelContentProps {
 }
 
 const LabelContent = ({ text, loading = false, optional = false, required = false }: LabelContentProps) => {
+  const { t } = useTranslation();
+
   if (!text) return null;
 
   const requiredText = typeof required === 'string' ? required : undefined;
@@ -20,11 +23,11 @@ const LabelContent = ({ text, loading = false, optional = false, required = fals
       {loading && <Spinner aria-label="spinning" data-size="xs" className={style.loading} />}
       {required && requiredText && (
         <Tag className={`${style.spacing} ${style.tag}`} data-color="warning">
-          {requiredText ?? 'Må fylles ut'}
+          {requiredText}
         </Tag>
       )}
       {required && !requiredText && <span className={style.requiredStar}>*</span>}
-      {optional && <Tag className={`${style.spacing} ${style.tag}`}>{optionalText ?? 'Valgfritt'}</Tag>}
+      {optional && <Tag className={`${style.spacing} ${style.tag}`}>{optionalText ?? t('labelContent.optional')}</Tag>}
     </span>
   );
 };
