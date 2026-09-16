1ee33b5d3cc21fa6597422796ceaa487cb18bd04
eksepsjon <380919+eksepsjon@users.noreply.github.com>
2026-09-16 10:10:28 +0000

    Deploying to gh-pages from @ Kystverket/styrbord@eb3f23183c60461d93dcc7e573c5e32ccfa3839f 🚀

---
+++ base/assets/errorLabel.stories-CPMmKygB.js
@@ -0,0 +1,8 @@
+import{S as i,j as n}from"./iframe-WKIRg0XG.js";import{W as d}from"./Dialog-D4qjwnD9.js";import"./preload-helper-Dp1pzeXC.js";import"./tooltip-Cb0cksK0.js";import"./index-BprNiIUZ.js";import"./index-B46LPQkM.js";import"./shipTypes-hFnSBndP.js";import"./Details-CRHO2nlV.js";import"./KyvDivider-L4aYN3Mp.js";import"./typography-DVDzzXfp.js";import"./KyvSpinner-DMTidkHX.js";import"./skillingsbuoye-DkUwV7IG.js";import"./Logo-DF4wfcVh.js";import"./color-tokens-DRys5hYJ.js";const _={title:"Deprecated/ErrorLabel",component:d,decorators:[i],tags:["autodocs","deprecated"],argTypes:{}},c={error:"I am an error",children:n.jsx("input",{type:"text"})},r={args:c},o={args:{...c,error:void 0}};var e,t,a;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: defaultProps
+}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};var s,p,m;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    ...defaultProps,
+    error: undefined
+  }
+}`,...(m=(p=o.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const h=["Default","NoError"];export{r as Default,o as NoError,h as __namedExportsOrder,_ as default};
